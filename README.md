92942e03e5e75f24b111b07dc9a9e4e4b3a8680f
heisalien <hejacky28@gmail.com>
2013-04-20 02:19:34 -0400

    Initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# qidi one month rails
+this is the study sample from qidi
+
+by [qidiiiiiiiiii]
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App1::Application.config.secret_token = '18067ae664196a77f766b8e12c3895fdb060bceba0b36bd340e8c88d4833c286d0b96ead316315cef7c55ee5fd12d0a8892c0be2f6f300626d2a7007bff6cdea'
